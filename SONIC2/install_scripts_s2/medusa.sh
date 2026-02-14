f7fafe31792e355be914f562082bfcb0e2d66364
WillianJunior <guns945@gmail.com>
2026-02-13 21:01:15 -0300

    Update medusa.sh

--- SONIC2/install_scripts_s2/medusa.sh
+++ SONIC2/install_scripts_s2/medusa.sh
@@ -1,4 +1,10 @@
 # tested on medusa5
+# BAD:
+# 1. using static IP on SPEED
+# 2. using CRC IP for internet
+# 3. mounting /home with /dev/mapper/rl_cisteina-scratch -> TODO: use the correct /home across all nodes
+# 4. mounting sonic_home -> TODO: should be /home
+# Change to ansible
 
 # Base ===================================================================
 dnf install -y epel-release
@@ -6,6 +12,7 @@ dnf update -y
 grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=1 psi=1"
 dnf groupinstall -y "Development Tools"
 dnf install -y htop cmake
+dnf install -y ansible
 
 # TODO: configure htop globally with PSI (PSI already showing)
 
@@ -16,15 +23,15 @@ systemctl restart sshd.service
 
 
 # Network ================================================================
-ME=medusa5
-MY_IP_SPEED=192.168.62.101
-NET_INTF=eno2 # check correct interface
-GATEWAY_SPEED=192.168.62.254
+export ME=medusa5
+export MY_IP_SPEED=192.168.62.101
+export NET_INTF=eno2
+export GATEWAY_SPEED=192.168.62.254
 
 hostnamectl set-hostname $ME
 nmcli con mod $NET_INTF +ipv4.addresses ${MY_IP_SPEED}/24
 nmcli con mod $NET_INTF +ipv4.routes "192.168.62.0/24 $GATEWAY_SPEED"
-nmcli con mod $NET_INTF ipv4.method manual
+nmcli con mod $NET_INTF ipv4.method auto # currently the 192.168.62.0/24 network doesn't forward to the internet, thus the CRC IP is needed.
 nmcli con mod $NET_INTF connection.autoconnect yes
 nmcli con down $NET_INTF && nmcli con up $NET_INTF
 
@@ -58,30 +65,38 @@ systemctl enable --now sssd
 
 
 # Mount global storages ==================================================
+dnf install -y  nfs-utils
 
-# mount storages
-#  - mount sonic_...
-#  - mount sonic_home also as /home
+mkdir /sonic_etc
+mkdir /sonic_modules
+mkdir /sonic_home
+
+echo "192.168.62.100:/nfs/exports/sonic_etc /sonic_etc nfs defaults,_netdev 0 0" >> /etc/fstab
+echo "192.168.62.100:/nfs/exports/sonic_modules /sonic_modules nfs defaults,_netdev 0 0" >> /etc/fstab
+echo "192.168.62.100:/nfs/exports/sonic_home /sonic_home nfs defaults,_netdev 0 0" >> /etc/fstab
+
+systemctl daemon-reload
+mount -a
+
 
 # Rework local storage ===================================================
-# Destroy previous /home volume
 umount /home
 vim /etc/fstab # remove /home mount line
 lvdisplay # to find the name of the home logical volume
 lvchange -an /dev/rl_cisteina/home # deactivate volume
 lvremove /dev/rl_cisteina/home # kill volume
-
-# Create and format new volumes
 lvcreate -n scratch -L 1T rl_cisteina
 lvcreate -n storage rl_cisteina -l 100%FREE
-mkfs.ext4 /dev/rl_cisteina/scratch
-mkfs.ext4 /dev/rl_cisteina/storage
+
+# Format volumes
+mkfs.xfs /dev/rl_cisteina/scratch
+mkfs.xfs /dev/rl_cisteina/storage
 
 # Mount volumes
 mkdir /scratch
 mkdir /storage
-echo '/dev/rl_cisteina/scratch   /scratch   ext4   defaults   0 0' >> /etc/fstab
-echo '/dev/rl_cisteina/storage   /storage   ext4   defaults   0 0' >> /etc/fstab
+echo '/dev/rl_cisteina/scratch   /scratch   xfs   defaults   0 0' >> /etc/fstab
+echo '/dev/rl_cisteina/storage   /storage   xfs   defaults   0 0' >> /etc/fstab
 systemctl daemon-reload
 mount -a
 
@@ -93,22 +108,21 @@ tee /etc/profile.d/modules.sh > /dev/null <<'EOF'
 if [ -f /usr/share/Modules/init/bash ]; then
     source /usr/share/Modules/init/bash
 fi
+export MODULEPATH=/sonic_modules/dcc-sonic-modules/modulefiles
 EOF
 
-dnf install -y ansible
 
-# modules
-#  - install ansible and modules environment
-#  - install modules from ansible
-#  - test
+modules
+ - install modules from ansible
+ - test
 
 
 # Slurm ==================================================================
 
-# slurm client
-#  - munge
-#  - slurmd
+slurm client
+ - munge
+ - slurmd
 
-# slurm PAM
+slurm PAM
 
-# GRES 1 GPU
+GRES 1 GPU
