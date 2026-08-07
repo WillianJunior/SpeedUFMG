f93728a4a152f01896089733eac544f6ab6440c1
WillianJunior <guns945@gmail.com>
2026-08-07 13:07:15 -0300

    Update update_cuda_driver.sh

--- SONIC2/scripts/update_cuda_driver.sh
+++ SONIC2/scripts/update_cuda_driver.sh
@@ -9,3 +9,17 @@ module load cuda/12.8.0
 make clean
 make
 ./transpose
+
+
+
+# medusas:
+dnf update -y
+reboot
+nvidia-uninstall
+reboot
+dnf clean all
+dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel9/x86_64/cuda-rhel9.repo
+dnf makecache
+dnf install -y nvidia-driver.x86_64
+reboot
+dnf install -y nvidia-driver-cuda
